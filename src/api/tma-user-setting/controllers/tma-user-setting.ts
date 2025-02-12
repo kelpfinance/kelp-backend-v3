ccad4962553b292fe9447bf19b2388112a7e8f25
Ali <akbar.alizadeh5@gmail.com>
2025-02-12 11:00:37 -0800

    tma settings (#5)

---
+++ src/api/tma-user-setting/controllers/tma-user-setting.ts
@@ -0,0 +1,7 @@
+/**
+ * tma-user-setting controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tma-user-setting.tma-user-setting');
---
+++ src/api/tma-user-setting/routes/tma-user-setting.ts
@@ -0,0 +1,7 @@
+/**
+ * tma-user-setting router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::tma-user-setting.tma-user-setting');
